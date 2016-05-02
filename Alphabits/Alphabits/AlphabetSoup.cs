6048c1eee15a44564d3275a876d0ee6d95b5b6b1
Jason Weakley <jason.b.weakley@gmail.com>
2016-05-02 14:44:12 -0500

    refactored

--- Alphabits/Alphabits/AlphabetSoup.cs
+++ Alphabits/Alphabits/AlphabetSoup.cs
@@ -4,28 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// accept only alpha chars, and only once per alpha, until all letters A-Z are entered
-
-// count the number of correct characters typed, and output number to console
-
-// compare typed char to list of A-Z: if typed char matches this list and is not in 
-// user's list of already typed chars add it to the user's list. 
-
-// if a typed char does not match A-Z list output message that char is not a letter A-Z
-
-// if typed char matches user's list output message saying char has been typed already
-
-// Use one Class in addition to the program file.
-
-// Use the String.Format method to craft your messages to users.
-
-// Your class should include a constructor function, an "addChar" method, and a "listLength" method.
-
-// I think this will be needed: 
-// https://msdn.microsoft.com/en-us/library/system.windows.forms.keypresseventargs.keychar(v=vs.110).aspx
-
-// Char.ToLower is a method for turning caps to lower, if not a char or already lower, no change
-
 namespace Alphabits
 {
   public class AlphabetSoup
@@ -40,6 +18,7 @@ namespace Alphabits
             {
                 isInList(lowerCase);
             }
+           
             else
             {
                 Console.Clear();
--- Alphabits/Alphabits/Program.cs
+++ Alphabits/Alphabits/Program.cs
@@ -22,51 +22,49 @@ namespace Alphabits
                     string output = String.Format("Type all 26 letters of the alphabet.");
                     Console.WriteLine(output);
                 }
-                
+
                 // assign whatever is typed to a string called entry
                 string entry = Console.ReadLine();
 
-                // get the first char of whatever the user types and test if a letter using isChar method from AlphabetSoup
-                char entryChar = entry[0];
-                buildUserAlpha.isChar(entryChar);
-
-                string counter = String.Format("You have entered {0} of 26 letters.",
-                   buildUserAlpha.userAlphabet.Count);
-
-                if (buildUserAlpha.listLength()==26)
+                // handle null exception for entry
+                if (entry == String.Empty)
                 {
                     Console.Clear();
-                    string congrats = String.Format("CONGRATULATIONS! You have finished the alphabet!");
-                    Console.WriteLine(congrats);
-                    Console.ReadLine();
-                    ABCs = false;
-                }
-                else if (buildUserAlpha.listLength()>=1)
-                {
-                    Console.Clear();
-                    counter += String.Format(" Keep going!");
-                    Console.WriteLine(counter);
+                    Console.WriteLine("Your entry was blank. Type a letter and press enter to continue.");
+                    entry = "0";
+                    continue;
                 }
                 else
                 {
-                    Console.Clear();
-                    Console.WriteLine(counter);
-                    continue;
+                    // get the first char of whatever the user types and test if a letter using isChar method from AlphabetSoup
+                    char entryChar = entry[0];
+                    buildUserAlpha.isChar(entryChar);
+
+                    string counter = String.Format("You have entered {0} of 26 letters.",
+                       buildUserAlpha.userAlphabet.Count);
+
+                    if (buildUserAlpha.listLength() == 26)
+                    {
+                        Console.Clear();
+                        string congrats = String.Format("CONGRATULATIONS! You have finished the alphabet!");
+                        Console.WriteLine(congrats);
+                        Console.ReadLine();
+                        ABCs = false;
+                    }
+                    else if (buildUserAlpha.listLength() >= 1)
+                    {
+                        Console.Clear();
+                        counter += String.Format(" Keep going!");
+                        Console.WriteLine(counter);
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        Console.WriteLine(counter);
+                        continue;
+                    }
                 }
-               
             }
-
-
-
-            //var allUserInput = "";
-
-
-
-            //AlphabetSoup alpha = new AlphabetSoup();
-
-            //var alphaBET = alpha.WriteAlphabet();
-            //Console.WriteLine(alphaBET);
-            //Console.ReadLine();
         }
     }
 }
